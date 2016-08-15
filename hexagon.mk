e9ed1b1d1a9baa5c9642ba8e2190c31668eebd15
Alex Mihalenok <alex.gibson@bk.ru>
2016-08-15 10:33:43 +0300

    Little fix 2

--- hexagon.mk
+++ hexagon.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/sony/odin/full_odin.mk)
 $(call inherit-product, vendor/hexagon/configs/nfc_enhanced.mk)
 
 # Inherit hexagon common Phone stuff.
-$(call inherit-product, vendor/hexagon/configs/common)
+$(call inherit-product, vendor/hexagon/configs/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.1.1/10.7.A.0.222/4076033025:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.1.1 10.7.A.0.222 4076033025 test-keys"
 
